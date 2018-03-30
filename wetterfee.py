18dd45538a2903aafc6d13e105d846ee4fd2da03
gk <gabriel.kabbe@mail.de>
2018-03-30 14:14:18 +0200

    Rename main -> wetterfee

--- wetterfee.py
+++ wetterfee.py
@@ -1,38 +1,34 @@
 #!/usr/bin/python3
 
-import numpy as np
-import pandas as pd
-import matplotlib.pylab as plt
-from ftplib import FTP
-import zipfile
-from io import StringIO
-import ipdb
-import sys, os
+import os
 import re
+import zipfile
+from ftplib import FTP
+
+import pandas as pd
 
 
 class DataManager:
 
     server_adress = "ftp-cdc.dwd.de"
     filepath = "/pub/CDC/observations_germany/climate/daily/kl/historical/"
-    description_file = "KL_Tageswerte_Beschreibung_Stationen.txt" # hier stehen die Wetterstationen drin
-
+    # hier stehen die Wetterstationen drin
+    description_file = "KL_Tageswerte_Beschreibung_Stationen.txt"
     def __init__(self):
- 
         self.ftp = None
         self.lookup = self.get_file(self.description_file)
 
     def _connect_to_ftp(self):
         self.ftp = FTP(self.server_adress)
         self.ftp.login()
-        
+
     def _download(self, fname):
         if not self.ftp:
             self._connect_to_ftp()
         self.ftp.cwd(self.filepath)
         with open(fname, "bw") as f:
             self.ftp.retrbinary("RETR "+fname, f.write)
-         
+
     def get_file(self, fname):
         try:
             with open(fname, "r", encoding="ISO-8859-1") as f:
@@ -43,42 +39,38 @@ class DataManager:
             print("Trying to download")
             self._download(fname)
             return self.get_file(fname)
-            
+
     def get_zipfile(self, fname):
         try:
             zf = zipfile.ZipFile(fname)
-            for n in zf.namelist():
-                if re.search("produkt_klima_Tageswerte", n):
-                    print("Found", n)
-                    with zf.open(n, "r") as f:
-                        data = pd.read_csv(f, sep=b"\s*;\s*")
+            for fname in zf.namelist():
+                if re.search("produkt_klima_Tageswerte", fname):
+                    print("Found", fname)
+                    with zf.open(fname, "r") as f:
+                        data = pd.read_csv(f, sep=";", parse_dates=[1], encoding="utf-8")
+                        data.columns = data.columns.str.strip()
                     return data
         except IOError:
             print("file not found")
             print("Trying to download")
             self._download(fname)
             return self.get_zipfile(fname)
-                   
+
     def test(self):
         for line in self.lookup:
             print(line)
-        
-    def get_station_number(self, city_name):
+
+    def get_station_numbers(self, city_name):
         hits = []
         for line in self.lookup:
             if re.search(city_name, line, flags=re.I):
                 hits.append((line.split()[0], line.split()[6]))
-        if len(hits) > 1:
-            for num, name in hits:
-                print(name)
-            raise NameError("Multiple hits. Specify your request")
-        elif len(hits) == 0:
-            raise NameError("No hit at all.")
-        return hits.pop()
-       
+        return hits
+
     def get_weather_data(self, station_id):
+        station_id = str(station_id)
         # Check first if file has already been downloaded
-        for f in os.listdir("."):
+        for f in os.listdir(os.curdir):
             if re.search("tageswerte", f) and station_id in re.findall("tageswerte_0*(\d+)", f)[0]:
                 print("Found matching file:")
                 print(f)
@@ -92,22 +84,10 @@ class DataManager:
             print("Looking for", station_id)
             for line in station_names:
                 if re.search("tageswerte", line):
-                    if station_id == re.findall("tageswerte_0*(\d+)", line)[0]:
+                    match = re.findall("tageswerte_0*(\d+)", line)
+                    if match and station_id == match[0]:
                         print("Found matching file:")
                         print(line)
                         fname = line.split()[-1]
                         self.get_file(fname)
         return self.get_zipfile(fname)
-                        
-
-def main(*args):
-    dm = DataManager()
-    num, name = dm.get_station_number("kröllwitz")
-    x = dm.get_weather_data(num)
-    ipdb.set_trace()
-
-if __name__ == "__main__":
-    main()
-
-
-
